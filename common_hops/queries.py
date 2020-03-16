e3385c62ab4418216ce0a25584414105e6288b59
963 <manjaritrivedi96@gmail.com>
2020-03-16 12:43:16 +0000

    Added date parameter (along with before and after functions), and added functionality for multiple (2+) pairs. Still some work with before/after

--- common_hops/queries.py
+++ common_hops/queries.py
@@ -1,4 +1,4 @@
-def common_hops(es, host1, host2, host3, host4, ipv6):
+def find_path(es, host1, host2, ipv6, date, pair):
     """
     Get list of hops for
     host1 : Source 1 (String)
@@ -6,13 +6,19 @@ def common_hops(es, host1, host2, host3, host4, ipv6):
     host3 : Source 2 (String)
     host4: Destination 2(String) 
     ipv6: (Bool)
-    """
-    
-    query1 = {
+    """    
+    querya1 = {
       "size": 0,
       "query": {
         "bool": {
           "filter": [
+            {
+              "range": {
+                "timestamp": {
+                  "gte": date
+                }
+              }
+            },
             {
               "term": {
                 "src_host": {
@@ -38,23 +44,72 @@ def common_hops(es, host1, host2, host3, host4, ipv6):
         }
       },
         "aggs": {
-            "the_hash": {
-              "terms": {
-                "field": "route-sha1",
-                "order": {"_count" : "desc"},
-                "size": 1
+            "after_time": {
+              "min": {
+                "field": "timestamp"
               }
             }
           }
         }
     
-    data1 = es.search('ps_trace', body=query1)  
-    if not data1['aggregations']['the_hash']['buckets']:
-        print("no such record, 1st pair")
+    dataa1 = es.search('ps_trace', body=querya1)  
+    if not dataa1['aggregations']['after_time']['value']:
+        print("time after error")
         return
-    hash1 = data1['aggregations']['the_hash']['buckets'][0]['key']
+    timea = dataa1['aggregations']['after_time']['value']
     
-    query2 = {
+    queryb1 = {
+      "size": 0,
+      "query": {
+        "bool": {
+          "filter": [
+             {
+              "range": {
+                "timestamp": {
+                  "lte": date
+                }
+              }
+            },
+            {
+              "term": {
+                "src_host": {
+                  "value": host1
+                }
+              }
+            },
+            {
+              "term": {
+                "dest_host": {
+                  "value": host2
+                }
+              }
+            },
+              {
+              "term": {
+                "ipv6": {
+                  "value": ipv6
+                }
+              }
+            }
+          ]
+        }
+      },
+        "aggs": {
+            "before_time": {
+              "max": {
+                "field": "timestamp"
+              }
+            }
+          }
+        }
+    
+    datab1 = es.search('ps_trace', body=queryb1)  
+    if not datab1['aggregations']['before_time']['value']:
+        print("time before error")
+        return
+    timeb = datab1['aggregations']['before_time']['value']
+  
+    query11 = {
       "size": 0,
       "query": {
         "bool": {
@@ -62,18 +117,25 @@ def common_hops(es, host1, host2, host3, host4, ipv6):
             {
               "term": {
                 "src_host": {
-                  "value": host3
+                  "value": host1
                 }
               }
             },
             {
               "term": {
                 "dest_host": {
-                  "value": host4
+                  "value": host2
                 }
               }
             },
-              {
+            {
+              "term": {
+                "timestamp": {
+                  "value": str(timea)
+                }
+              }
+            },
+            {
               "term": {
                 "ipv6": {
                   "value": ipv6
@@ -93,14 +155,76 @@ def common_hops(es, host1, host2, host3, host4, ipv6):
             }
           }
         }
-    data2 = es.search('ps_trace', body=query2)  
-    if not data2['aggregations']['the_hash']['buckets']:
-        print("no such record, 2nd pair")
+    
+    data11 = es.search('ps_trace', body=query11)  
+    if not data11['aggregations']['the_hash']['buckets']:
+        print("no such record, pair " + str(pair))
         return
-    hash2 = data2['aggregations']['the_hash']['buckets'][0]['key']
-
-
-    query3 = {
+    hash11 = data11['aggregations']['the_hash']['buckets'][0]['key']
+    
+    query12 = {
+      "size": 0,
+      "query": {
+        "bool": {
+          "filter": [
+            {
+              "term": {
+                "src_host": {
+                  "value": host1
+                }
+              }
+            },
+            {
+              "term": {
+                "dest_host": {
+                  "value": host2
+                }
+              }
+            },
+            {
+              "term": {
+                "timestamp": {
+                  "value": str(timeb)
+                }
+              }
+            },
+            {
+              "term": {
+                "ipv6": {
+                  "value": ipv6
+                }
+              }
+            }
+          ]
+        }
+      },
+        "aggs": {
+            "the_hash": {
+              "terms": {
+                "field": "route-sha1",
+                "order": {"_count" : "desc"},
+                "size": 1
+              }
+            }
+          }
+        }
+    
+    data12 = es.search('ps_trace', body=query12)  
+    if not data12['aggregations']['the_hash']['buckets']:
+        print("no such record, pair " + str(pair))
+        return
+    hash12 = data12['aggregations']['the_hash']['buckets'][0]['key']
+    
+    if (hash11 == hash12):
+        same = True
+        print("paths before and after input time are same for pair " + str(pair))
+    else:
+        same = False
+        print("paths before and after input time differ for pair " + str(pair))
+        #TODO: Ask user which paths they require
+        #As of now, getting both paths
+    
+    query31 = {
       "size": 0,
       "query": {
         "bool": {
@@ -108,7 +232,7 @@ def common_hops(es, host1, host2, host3, host4, ipv6):
             {
               "term": {
                 "route-sha1": {
-                  "value": hash1
+                  "value": hash11
                 }
               }
             }
@@ -123,58 +247,75 @@ def common_hops(es, host1, host2, host3, host4, ipv6):
             }
           }
         }
-    raw1 = es.search('ps_trace', body=query3) 
+    raw11 = es.search('ps_trace', body=query31) 
         
-    hops1 = raw1['aggregations']['hops_list']['hits']['hits'][0]['_source']['hops']
-    the_hops1 = []
+    hops11 = raw11['aggregations']['hops_list']['hits']['hits'][0]['_source']['hops']
+    the_hops11 = []
     
-    for x in range(len(hops1) - 1):
-        the_hops1.append([hops1[x], hops1[x+1], x])
+    for x in range(len(hops11) - 1):
+        the_hops11.append([hops11[x], hops11[x+1], x])
         
-    
-    query4 = {
-      "size": 0,
-      "query": {
-        "bool": {
-          "filter": [
-            {
-              "term": {
-                "route-sha1": {
-                  "value": hash2
+    if (same == False):
+        
+        query32 = {
+          "size": 0,
+          "query": {
+            "bool": {
+              "filter": [
+                {
+                  "term": {
+                    "route-sha1": {
+                      "value": hash12
+                    }
+                  }
+                }
+              ]
+            }
+          },
+            "aggs": {
+                "hops_list": {
+                  "top_hits": {
+                    "size": 1
+                  }
                 }
               }
             }
-          ]
-        }
-      },
-        "aggs": {
-            "hops_list": {
-              "top_hits": {
-                "size": 1
-              }
-            }
-          }
-        }
-    raw2 = es.search('ps_trace', body=query4) 
-    hops2 = raw2['aggregations']['hops_list']['hits']['hits'][0]['_source']['hops']
-    the_hops2 = []
-    
-    for x in range(len(hops2) - 1):
-        the_hops2.append([hops2[x], hops2[x+1], x])
+        raw12 = es.search('ps_trace', body=query32) 
         
+        hops12 = raw12['aggregations']['hops_list']['hits']['hits'][0]['_source']['hops']
+        the_hops12 = []
     
-        
-    common_hops = []   
+        for x in range(len(hops12) - 1):
+            the_hops12.append([hops12[x], hops12[x+1], x])
     
+    #Make less redundant? (wrt before/after queries similar)
+    #TODO: Return the data!! (once figured out how to where to handle before/after; are we doing both, only one??)
+    
+    
+def common_hops(es, ipv6, date, *sites):
+    if (len(sites) % 2 == 1):
+        print("Error: sites must be in host dest PAIRS")
+        return
+    n = 0
+    pair = 1
+    while n < len(sites):
+        find_path(es, sites[n], sites[n+1], ipv6, date, pair) #TODO: put this data into array
+        pair += 1 
+        n += 2
+    
+    return #for now
+    #TODO: Generalised method for comparing paths; old one for two pairs below and DOES NOT WORK NOW
+    common_hops_arr = []   
+
     for x in range(len(hops1) - 1):
         for y in range(len(hops2) - 1):
             if ((the_hops1[x][0] == the_hops2[y][0]) and (the_hops1[x][1] == the_hops2[y][1])):
                 the_hops1[x].append(the_hops2[y][2])
-                common_hops.append(the_hops1[x])
-                
-    
-    print(len(common_hops))
-    for x in range(len(common_hops)):
-        print(common_hops[x][0], ", ", common_hops[x][1], ", ", common_hops[x][2], ", ", common_hops[x][3])
+                common_hops_arr.append(the_hops1[x])
+
+
+    print(len(common_hops_arr))
+    for x in range(len(common_hops_arr)):
+        print(common_hops_arr[x][0], ", ", common_hops_arr[x][1], ", ", common_hops_arr[x][2], ", ", common_hops_arr[x][3])
     
      
